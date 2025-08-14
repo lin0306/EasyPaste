f13a3eca66cca1be4c50f6791ef734076e2070bb
linweijia <2138388112@qq.com>
2025-08-14 11:00:36 +0800

    fix：按需引入不生效

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import naive from 'naive-ui';
-import { createPinia } from "pinia";
-import { createApp } from "vue";
+import {createPinia} from "pinia";
+import {createApp} from "vue";
 import App from "./App.vue";
 // 等宽字体
 import 'vfonts/FiraCode.css';
@@ -8,24 +7,23 @@ import router from "./routers";
 
 // 全局滚动条样式设置
 import './assets/css/scrollbarGlobal.css';
-import { useLanguage } from './configs/LanguageConfig';
-import { useTheme } from './configs/ThemeConfig';
+import {useLanguage} from './configs/LanguageConfig';
+import {useTheme} from './configs/ThemeConfig';
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(naive);
 app.use(pinia);
 app.use(router);
 
 // 在挂载前初始化主题和语言
-const { initializeTheme } = useTheme()
-const { initializeLanguage } = useLanguage()
+const {initializeTheme} = useTheme()
+const {initializeLanguage} = useLanguage()
 
 initializeTheme().then(() => {
-  initializeLanguage().then(() => {
-    app.mount('#app')
-  })
+    initializeLanguage().then(() => {
+        app.mount('#app')
+    })
 })
 
 
