d7df49d7f6ef5f06a980e887dc32243b10c017bc
linweijia <2138388112@qq.com>
2025-08-19 14:31:17 +0800

    程序日志保留时间改成20天

--- src-tauri/src/log.rs
+++ src-tauri/src/log.rs
@@ -14,7 +14,7 @@ pub fn init<R: Runtime>() -> TauriPlugin<R> {
         ])
         .max_file_size(50_000 /* bytes */) // 单个日志文件大小限制为 50KB
         .level(log::LevelFilter::Info) // 日志级别
-        .rotation_strategy(RotationStrategy::KeepAll) // 设置日志保留策略为保留所有日志文件
+        .rotation_strategy(RotationStrategy::KeepSome(30)) // 设置日志保留策略为保留所有日志文件
         .timezone_strategy(TimezoneStrategy::UseLocal) // 日志时区使用本地时区
         .format(|out, message, record| {
             out.finish(format_args!(
