b4b0274265f942c03b931fe896adaaa1673ee9fb
linweijia <2138388112@qq.com>
2025-09-02 18:10:56 +0800

    按照文件类型不同，展示不同的文件图标

---
+++ src/assets/icons/PPTIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg t="1756804521696" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+       p-id="1271" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200">
+    <path
+        d="M667.36 68.96L656 64H291.36a59.68 59.68 0 0 0-61.6 57.44 50.08 50.08 0 0 0 0 8.48v146.08h-51.68a75.2 75.2 0 0 0-65.92 80V769.6h117.92V960H912V332.8z m-13.92 65.92l176 188.16h-144a38.08 38.08 0 0 1-35.84-28.32v-160zM160 355.68c0-11.68 4.8-32 18.72-32H512c13.92 0 23.36 20.96 23.36 32v366.88H160z m116.48 555.04V769.6h305.6V355.68a77.6 77.6 0 0 0-70.4-80H277.12V129.92c0-13.92 0-18.88 16-18.88h313.76v183.68a81.6 81.6 0 0 0 80 75.36h179.04v540.64z"
+        :fill="themeColors.text" p-id="1272"></path>
+    <path d="M253.28 652.32h47.04V440.32h94.08v94.08h-70.24v47.04h117.28V393.28H253.28v259.04z" :fill="themeColors.text"
+          p-id="1273"></path>
+  </svg>
+</template>
+<script lang="ts" setup>
+import {useTheme} from '../../services/ThemeService.ts';
+// 获取主题颜色
+const {themeColors} = useTheme();
+</script>
+<style>
+svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -75,3 +75,110 @@ function convertToBase64(arrayBuffer: any) {
     }
     return window.btoa(binary);
 }
+
+/**
+ * 判断文件是否是Word文件
+ * @param filePath 文件路径
+ */
+export function isWord(filePath: string) {
+    return filePath.toLowerCase().endsWith('.doc')
+        || filePath.toLowerCase().endsWith('.docx')
+        || filePath.toLowerCase().endsWith('.dotx')
+        || filePath.toLowerCase().endsWith('.dotm')
+        ;
+}
+
+/**
+ * 判断文件是否是excel文件
+ * @param filePath 文件路径
+ */
+export function isExcel(filePath: string) {
+    return filePath.toLowerCase().endsWith('.xlsx')
+        || filePath.toLowerCase().endsWith('.xls')
+        || filePath.toLowerCase().endsWith('.xlsm')
+        || filePath.toLowerCase().endsWith('.xlsb')
+        ;
+}
+
+/**
+ * 判断文件是否是ppt文件
+ * @param filePath 文件路径
+ */
+export function isPPT(filePath: string) {
+    return filePath.toLowerCase().endsWith('.pptx')
+        || filePath.toLowerCase().endsWith('.ppt')
+        || filePath.toLowerCase().endsWith('.potx')
+        || filePath.toLowerCase().endsWith('.potm')
+        || filePath.toLowerCase().endsWith('.pptm')
+        || filePath.toLowerCase().endsWith('.ppsx')
+        || filePath.toLowerCase().endsWith('.ppsm')
+        ;
+}
+
+/**
+ * 判断文件是否是pdf文件
+ * @param filePath 文件路径
+ */
+export function isPDF(filePath: string) {
+    return filePath.toLowerCase().endsWith('.pdf')
+        || filePath.toLowerCase().endsWith('.pdfa')
+        || filePath.toLowerCase().endsWith('.pdfx')
+        || filePath.toLowerCase().endsWith('.pdfua')
+        ;
+}
+
+/**
+ * 判断文件是否是文本文件
+ * @param filePath 文件路径
+ */
+export function isTXT(filePath: string) {
+    return filePath.toLowerCase().endsWith('.txt')
+        || filePath.toLowerCase().endsWith('.text')
+        || filePath.toLowerCase().endsWith('.log')
+        || filePath.toLowerCase().endsWith('.csv')
+        || filePath.toLowerCase().endsWith('.tsv')
+        || filePath.toLowerCase().endsWith('.md')
+        || filePath.toLowerCase().endsWith('.rtf')
+        ;
+}
+
+/**
+ * 判断文件是否是音频文件
+ * @param filePath 文件路径
+ */
+export function isAudio(filePath: string) {
+    return filePath.toLowerCase().endsWith('.mp3')
+        || filePath.toLowerCase().endsWith('.wav')
+        || filePath.toLowerCase().endsWith('.aac')
+        || filePath.toLowerCase().endsWith('.flac')
+        || filePath.toLowerCase().endsWith('.ogg')
+        || filePath.toLowerCase().endsWith('.wma')
+        ;
+}
+
+/**
+ * 判断文件是否是视频文件
+ * @param filePath 文件路径
+ */
+export function isVideo(filePath: string) {
+    return filePath.toLowerCase().endsWith('.mp4')
+        || filePath.toLowerCase().endsWith('.avi')
+        || filePath.toLowerCase().endsWith('.mkv')
+        || filePath.toLowerCase().endsWith('.mov')
+        || filePath.toLowerCase().endsWith('.wmv')
+        || filePath.toLowerCase().endsWith('.flv')
+        ;
+}
+
+/**
+ * 判断文件是否是压缩文件
+ * @param filePath 文件路径
+ */
+export function isPackage(filePath: string) {
+    return filePath.toLowerCase().endsWith('.zip')
+        || filePath.toLowerCase().endsWith('.rar')
+        || filePath.toLowerCase().endsWith('.7z')
+        || filePath.toLowerCase().endsWith('.tar')
+        || filePath.toLowerCase().endsWith('.gz')
+        ;
+}
