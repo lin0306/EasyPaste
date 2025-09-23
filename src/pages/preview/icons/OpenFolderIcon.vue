8388b1743e2411b9ff1ea880c30156f1bb2528ce
linweijia <2138388112@qq.com>
2025-09-23 20:00:33 +0800

    文件夹预览

---
+++ src/pages/preview/icons/OpenFolderIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg t="1758619187682" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+       p-id="6866"
+       xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20">
+    <path
+        d="M800 256H430.976L367.68 144.224A32 32 0 0 0 339.84 128H64a32 32 0 0 0-32 32v704a32 32 0 0 0 61.888 11.52L245.984 480H800a32 32 0 0 0 32-32V288a32 32 0 0 0-32-32z"
+        fill="#E8A113" p-id="6867"></path>
+    <path
+        d="M986.368 429.888A32 32 0 0 0 960 416H224a32 32 0 0 0-29.888 20.48l-160 416A32 32 0 0 0 64 896h736a32.032 32.032 0 0 0 29.856-20.48l160-416a32.064 32.064 0 0 0-3.488-29.632z"
+        fill="#FFC431" p-id="6868"></path>
+  </svg>
+</template>
+<script lang="ts" setup>
+</script>
+<style>
+svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
