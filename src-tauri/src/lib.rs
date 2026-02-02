4eef60ec346460819b27a3d169d99d725feb679f
linweijia <2138388112@qq.com>
2026-02-02 17:58:24 +0800

    增加控制台打印

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -74,6 +74,7 @@ pub fn run() {
 #[tauri::command]
 fn open_dev_tool(app_handle: tauri::AppHandle, window_name: &str) {
     if let Some(window) = app_handle.get_webview_window(&window_name) {
+        println!("正在打开 {} 页面的开发者工具", window_name);
         window.open_devtools();
     } else {
         eprintln!("找不到名称为 {} 的窗口", window_name);
