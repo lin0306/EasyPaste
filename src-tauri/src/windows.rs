865553ecd97d00b7d88927b3fcbd6c53bb57909a
linweijia <2138388112@qq.com>
2026-03-08 21:12:44 +0800

    fix：mac打包失败

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -11,50 +11,102 @@ pub fn create_main_window(app: AppHandle) {
 
     match file::load_file_content::<Settings>(app.clone(), "settings.json".into()) {
         Ok(data) => {
-            let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
-                .title("EasyPaste")
-                .inner_size(350.0, 550.0) // 窗口初始大小
-                .min_inner_size(350.0, 550.0) // 窗口最小大小
-                .max_inner_size(800.0, 1000.0) // 窗口最大大小
-                .prevent_overflow() // 限制窗口内容超出范围
-                .always_on_top(data.auto_hide_window || data.always_on_top) // 窗口置顶
-                .decorations(false) // 窗口无边框
-                .visible(false) // 窗口默认不显示
-                .skip_taskbar(data.auto_hide_window) // 任务栏不显示
-                .focused(true) // 窗口聚焦
-                .fullscreen(false) // 禁止全屏
-                .incognito(true) // 隐私模式
-                .shadow(true) // 窗口阴影
-                .content_protected(false) // 禁止窗口被捕获
-                .resizable(true) // 窗口可自由拖放大小
-                .disable_drag_drop_handler() // 允许拖拽
-                .transparent(true) // 窗口透明
-                .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
-                .build();
-            app.manage(window);
+            #[cfg(target_os = "windows")]
+            {
+                let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
+                    .title("EasyPaste")
+                    .inner_size(350.0, 550.0) // 窗口初始大小
+                    .min_inner_size(350.0, 550.0) // 窗口最小大小
+                    .max_inner_size(800.0, 1000.0) // 窗口最大大小
+                    .prevent_overflow() // 限制窗口内容超出范围
+                    .always_on_top(data.auto_hide_window || data.always_on_top) // 窗口置顶
+                    .decorations(false) // 窗口无边框
+                    .visible(false) // 窗口默认不显示
+                    .skip_taskbar(data.auto_hide_window) // 任务栏不显示
+                    .focused(true) // 窗口聚焦
+                    .fullscreen(false) // 禁止全屏
+                    .incognito(true) // 隐私模式
+                    .shadow(true) // 窗口阴影
+                    .content_protected(false) // 禁止窗口被捕获
+                    .resizable(true) // 窗口可自由拖放大小
+                    .disable_drag_drop_handler() // 允许拖拽
+                    .transparent(true) // 窗口透明
+                    .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
+                    .build();
+                app.manage(window);
+            }
+            #[cfg(target_os = "macos")]
+            {
+                let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
+                    .title("EasyPaste")
+                    .inner_size(350.0, 550.0) // 窗口初始大小
+                    .min_inner_size(350.0, 550.0) // 窗口最小大小
+                    .max_inner_size(800.0, 1000.0) // 窗口最大大小
+                    .prevent_overflow() // 限制窗口内容超出范围
+                    .always_on_top(data.auto_hide_window || data.always_on_top) // 窗口置顶
+                    .decorations(false) // 窗口无边框
+                    .visible(false) // 窗口默认不显示
+                    .skip_taskbar(data.auto_hide_window) // 任务栏不显示
+                    .focused(true) // 窗口聚焦
+                    .fullscreen(false) // 禁止全屏
+                    .incognito(true) // 隐私模式
+                    .shadow(true) // 窗口阴影
+                    .content_protected(false) // 禁止窗口被捕获
+                    .resizable(true) // 窗口可自由拖放大小
+                    .disable_drag_drop_handler() // 允许拖拽
+                    .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
+                    .build();
+                app.manage(window);
+            }
         }
         Err(_e) => {
-            let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
-                .title("EasyPaste")
-                .inner_size(350.0, 550.0) // 窗口初始大小
-                .min_inner_size(350.0, 550.0) // 窗口最小大小
-                .max_inner_size(800.0, 1000.0) // 窗口最大大小
-                .prevent_overflow() // 限制窗口内容超出范围
-                .always_on_top(true) // 窗口置顶
-                .decorations(false) // 窗口无边框
-                .visible(false) // 窗口默认不显示
-                .skip_taskbar(true) // 任务栏不显示
-                .focused(true) // 窗口聚焦
-                .fullscreen(false) // 禁止全屏
-                .incognito(true) // 隐私模式
-                .shadow(true) // 窗口阴影
-                .content_protected(false) // 禁止窗口被捕获
-                .resizable(true) // 窗口可自由拖放大小
-                .disable_drag_drop_handler() // 允许拖拽
-                .transparent(true) // 窗口透明
-                .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
-                .build();
-            app.manage(window);
+            #[cfg(target_os = "windows")]
+            {
+                let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
+                    .title("EasyPaste")
+                    .inner_size(350.0, 550.0) // 窗口初始大小
+                    .min_inner_size(350.0, 550.0) // 窗口最小大小
+                    .max_inner_size(800.0, 1000.0) // 窗口最大大小
+                    .prevent_overflow() // 限制窗口内容超出范围
+                    .always_on_top(true) // 窗口置顶
+                    .decorations(false) // 窗口无边框
+                    .visible(false) // 窗口默认不显示
+                    .skip_taskbar(true) // 任务栏不显示
+                    .focused(true) // 窗口聚焦
+                    .fullscreen(false) // 禁止全屏
+                    .incognito(true) // 隐私模式
+                    .shadow(true) // 窗口阴影
+                    .content_protected(false) // 禁止窗口被捕获
+                    .resizable(true) // 窗口可自由拖放大小
+                    .disable_drag_drop_handler() // 允许拖拽
+                    .transparent(true) // 窗口透明
+                    .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
+                    .build();
+                app.manage(window);
+            }
+            #[cfg(target_os = "macos")]
+            {
+                let window = WebviewWindow::builder(&app, "main", WebviewUrl::App(path))
+                    .title("EasyPaste")
+                    .inner_size(350.0, 550.0) // 窗口初始大小
+                    .min_inner_size(350.0, 550.0) // 窗口最小大小
+                    .max_inner_size(800.0, 1000.0) // 窗口最大大小
+                    .prevent_overflow() // 限制窗口内容超出范围
+                    .always_on_top(true) // 窗口置顶
+                    .decorations(false) // 窗口无边框
+                    .visible(false) // 窗口默认不显示
+                    .skip_taskbar(true) // 任务栏不显示
+                    .focused(true) // 窗口聚焦
+                    .fullscreen(false) // 禁止全屏
+                    .incognito(true) // 隐私模式
+                    .shadow(true) // 窗口阴影
+                    .content_protected(false) // 禁止窗口被捕获
+                    .resizable(true) // 窗口可自由拖放大小
+                    .disable_drag_drop_handler() // 允许拖拽
+                    .visible_on_all_workspaces(true) // 窗口在所有工作区都显示
+                    .build();
+                app.manage(window);
+            }
         }
     }
 }
